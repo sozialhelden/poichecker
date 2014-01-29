bf07edc0179bcbf70d93d239308c7272f9fd3b28
Christoph Bünte <info@christophbuente.de>
2014-01-29 13:05:55 +0100

    Configure geocoder for more than one backend service.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,13 @@
+require 'ostruct'
+require 'yaml'
+geocoder_config = YAML.load_file(Rails.root.join('config', 'geocoder.yml'))
+GeocoderConfig = OpenStruct.new geocoder_config[Rails.env]
+
 Geocoder.configure(
   # geocoding options
-  :timeout      => 60,           # geocoding service timeout (secs)
-  :lookup       => :nominatim,     # name of geocoding service (symbol)
-#  :lookup       => :google,     # name of geocoding service (symbol)
-  :language     => :de,         # ISO-639 language code
+  # :timeout      => 5,           # geocoding service timeout (secs)
+  :lookup       => :nominatim,    # name of geocoding service (symbol)
+  :language     => :de,           # ISO-639 language code
   # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
   # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
   # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
@@ -12,6 +16,15 @@ Geocoder.configure(
   # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
   :units => :km,                  # set default units to kilometers
 
+  # You can also configure multiple geocoding services at once, like this:
+  :nominatim => {
+     :timeout => 60
+   },
+
+   :here => {
+     :api_key => [GeocoderConfig.here["app_id"], GeocoderConfig.here["app_code"]],
+     :timeout => 5
+   },
 
   # exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
@@ -23,5 +36,5 @@ Geocoder.configure(
   # :distances => :linear    # :spherical or :linear
 
   # header information
-  :http_headers => { "User-Agent" => "Wheelmap v1.0, (http://wheelmap.org)" }
+  :http_headers => { "User-Agent" => "Poichecker v1.0, (http://poichecker.de)" }
 )
