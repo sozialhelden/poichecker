a1e496749b10cbcd30466563795ef563e46e9dec
Christoph Bünte <info@christophbuente.de>
2014-02-19 18:16:57 +0100

    Pluralize factory name.

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -10,5 +10,6 @@ FactoryGirl.define do
     password              "a_password"
     password_confirmation "a_password"
     osm_username          "a_osm_username"
+    role                  "admin"
   end
 end
