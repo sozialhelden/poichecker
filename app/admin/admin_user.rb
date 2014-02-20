6471ff7df0047cc881ca81a2d454944f40364598
Christoph Bünte <info@christophbuente.de>
2014-02-20 17:31:21 +0100

    Remove items from dataset view for non admins.

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -3,6 +3,8 @@ ActiveAdmin.register AdminUser do
 
   actions :all, :except => [:create]
 
+  permit_params :email, :osm_id, :osm_username, :role
+
   filter :email
   filter :osm_id
   filter :osm_username
@@ -15,5 +17,17 @@ ActiveAdmin.register AdminUser do
     column :role
     column :current_sign_in_at
     default_actions
+  end
+
+  form do |f|
+    f.inputs I18n.t('activerecord.models.admin_user.one') do
+      f.input :email
+      f.input :osm_id
+      f.input :osm_username
+      f.input :role
+    end
+    f.actions do
+      f.submit
+    end
   end
 end
--- app/admin/data_set.rb
+++ app/admin/data_set.rb
@@ -2,19 +2,22 @@
 ActiveAdmin.register DataSet do
   decorate_with DataSetDecorator
 
+  config.batch_actions = false
+
   permit_params :name, :license, :description
 
   filter :name
-  filter :license
-  filter :description
-  filter :created_at
+  filter :license,      if: ->(data_set) { current_admin_user.admin? }
+  filter :description,  if: ->(data_set) { current_admin_user.admin? }
+  filter :created_at,   if: ->(data_set) { current_admin_user.admin? }
 
   index :download_links => false do
-    selectable_column
-    column :id
+    selectable_column if current_admin_user.admin?
+    column :id if current_admin_user.admin?
     column :name
     column :license
     column :orte
+    column 'Gespendet am', :created_at
     default_actions if current_admin_user.admin?
 
   end
