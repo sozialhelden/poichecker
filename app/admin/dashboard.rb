8a2c63b13705fe34886208f4cd0c03e061dabccd
Christoph Bünte <info@christophbuente.de>
2014-02-27 14:58:05 +0100

    Move email field on dashboard to the top.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,6 +5,20 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     columns do
+      if current_admin_user.email.blank?
+        column do
+          active_admin_form_for(current_admin_user, :as => :admin_user, :url => admin_user_path(current_admin_user)) do |f|
+            f.inputs "Willkommen bei Poichecker" do
+              f.input :email, hint: "Damit informieren wir Dich über neue Datenspenden, die Deine Hilfe brauchen. Wir behandeln Deine Adresse vertraulich, kein Spam, versprochen."
+            end
+            f.actions do
+              f.submit "Update"
+            end
+          end
+        end
+      end
+    end
+    columns do
       column do
         panel "Info" do
           para "Hilf der freien Weltkarte OpenStreetMap: Kontrolliere Daten, die das Projekt gespendet bekommt. Das geht ganz leicht und macht sogar Spaß."
@@ -29,18 +43,7 @@ ActiveAdmin.register_page "Dashboard" do
           link_to "All Kommentare", comments_path
         end
       end
-      if current_admin_user.email.blank?
-        column do
-          active_admin_form_for(current_admin_user, :as => :admin_user, :url => admin_user_path(current_admin_user)) do |f|
-            f.inputs "Willkommen bei Poichecker" do
-              f.input :email, hint: "Damit informieren wir Dich über neue Datenspenden, die Deine Hilfe brauchen. Wir behandeln Deine Adresse vertraulich, kein Spam, versprochen."
-            end
-            f.actions do
-              f.submit "Update"
-            end
-          end
-        end
-      end
+
     end
   end # content
 end
