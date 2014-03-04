6a1a8fea5e7ba194fd72797a4a4563cf0ec2d33c
Christoph Bünte <info@christophbuente.de>
2014-03-04 10:53:51 +0100

    Use correct start/stop script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute '/etc/init.d/unicorn_matchy upgrade'
+      execute "/etc/init.d/poichecker_#{rails_env} upgrade"
     end
   end
 
