f383ae7a272a0025f6a729395694bcbd5fbe1209
Christoph Bünte <info@christophbuente.de>
2014-03-19 18:12:16 +0100

    Add poichecker logo.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -12,14 +12,14 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
   #
   # Note: Aim for an image that's 21px high so it fits in the header.
   #
-  # config.site_title_image = "logo.png"
+  config.site_title_image = "logo.png"
 
   # == Default Namespace
   #
