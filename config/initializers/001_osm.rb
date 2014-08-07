b1649c53a5fe2346b054b57f5d15d8acc1934aed
Christoph Bünte <info@christophbuente.de>
2014-08-07 14:57:06 +0200

    Make sure to use proper osm api base uri.

--- config/initializers/001_osm.rb
+++ config/initializers/001_osm.rb
@@ -2,4 +2,5 @@ require 'ostruct'
 require 'yaml'
 osm_config = YAML.load_file(Rails.root.join('config', 'osm.yml'))
 OpenStreetMapConfig = OpenStruct.new osm_config[Rails.env]
+Rosemary::Api.base_uri OpenStreetMapConfig.oauth_site
 
