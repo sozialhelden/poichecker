61388536c3387ab06aa1de8382825cf88de68a7f
Christoph Bünte <info@christophbuente.de>
2014-08-19 12:02:28 +0200

    Update rails from 4.0 to 4.1

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 group :default do
   # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-  gem 'rails', '4.0.9'
+  gem 'rails', '4.1.5'
 
   # Use postgresql as the database for Active Record
   gem 'pg'
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
 # # tell the I18n library where to find your translations
-I18n.enforce_available_locales = false
+I18n.enforce_available_locales = true
 
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :de
