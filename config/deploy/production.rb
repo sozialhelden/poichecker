8e7bf5d6b31c192b0162eb8b67e838cb297bf263
Christoph Bünte <info@christophbuente.de>
2014-08-20 17:14:48 +0200

    Run one worker for each queue on production environment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # Settings for delayed job
 set :delayed_job_server_role, :worker
-set :delayed_job_args, "-n 1 -p poichecker_production --queues=geocode,osm"
+set :delayed_job_args, "-p poichecker_production --pool=geocode --pool=osm"
 
 set :stage, :production
 set :deploy_to, '/var/apps/poichecker/production'
