402ee723191b44dad9a528765f541850ee5f9a65
Christoph Bünte <info@christophbuente.de>
2014-08-26 15:17:58 +0200

    Better mapping import.

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -2,11 +2,16 @@ class Mapping < ActiveRecord::Base
 
   def self.import(csv_file)
     CSV.parse(csv_file, headers: true, encoding: 'UTF-8', header_converters: :string) do |row|
-      Mapping.find_or_initialize_by(osm_key: row[:osm_key], osm_value: row[:osm_value], plural: row[:plural] ) do |mapping|
-        mapping.locale = row[:locale]
-        mapping.localized_name = row[:name]
-        mapping.save!
-      end
+      attribs = {
+        locale:         row[:locale],
+        localized_name: row[:name],
+        osm_key:        row[:osm_key],
+        osm_value:      row[:osm_value],
+        operator:       row[:operator],
+        plural:         row[:plural]
+      }
+
+      Mapping.find_or_create_by(attribs)
     end
   end
 
