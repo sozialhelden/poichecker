06117239ccbcb61611e643e8fd15edc9d594b39c
Christoph Bünte <info@christophbuente.de>
2015-12-03 17:26:10 +0100

    Disable login for poichecker.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Poichecker::Application.routes.draw do
+  # Disable login
+  get '/admin/login', to: redirect('/')
   devise_config = ActiveAdmin::Devise.config
   devise_config[:controllers][:omniauth_callbacks] = 'admin_users/omniauth_callbacks'
   devise_for :admin_users, devise_config
